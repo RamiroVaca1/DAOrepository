5f033809f7d15e7aea61dc866aff6cf53b06b41e
ramiro <ramirojvaca@gmail.com>
2022-09-19 12:29:06 -0300

    remove psw connection

--- src/main/java/com/solvd/hideConnections.java
+++
@@ -1,8 +0,0 @@
-package com.solvd;
-
-public class hideConnections {
-
-    public static String jdbc= "jdbc:mysql://localhost:3306/supermarket";
-    public static String root = "root";
-    public static String password = "tierrasperdidas123";
-}
